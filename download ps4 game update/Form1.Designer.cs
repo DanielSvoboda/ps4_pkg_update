6348461f242e6d619e838b0f696128474578d3c2
DanielSvoboda <daniel.svoboda@hotmail.com>
2024-07-10 16:01:15 -0300

    OK

---
+++ download ps4 game update/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace download_ps4_game_update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_TitleId = new System.Windows.Forms.TextBox();
+            this.label_TitleID = new System.Windows.Forms.Label();
+            this.textBox1_Result = new System.Windows.Forms.TextBox();
+            this.button_check_updates = new System.Windows.Forms.Button();
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key = new System.Windows.Forms.Label();
+            this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key = new System.Windows.Forms.TextBox();
+            this.linkLabel_github = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1_TitleId
+            // 
+            this.textBox1_TitleId.Location = new System.Drawing.Point(201, 31);
+            this.textBox1_TitleId.Name = "textBox1_TitleId";
+            this.textBox1_TitleId.Size = new System.Drawing.Size(70, 20);
+            this.textBox1_TitleId.TabIndex = 0;
+            // 
+            // label_TitleID
+            // 
+            this.label_TitleID.AutoSize = true;
+            this.label_TitleID.Location = new System.Drawing.Point(21, 34);
+            this.label_TitleID.Name = "label_TitleID";
+            this.label_TitleID.Size = new System.Drawing.Size(174, 13);
+            this.label_TitleID.TabIndex = 3;
+            this.label_TitleID.Text = "Title ID (in this format CUSA00001):";
+            // 
+            // textBox1_Result
+            // 
+            this.textBox1_Result.Location = new System.Drawing.Point(12, 57);
+            this.textBox1_Result.Multiline = true;
+            this.textBox1_Result.Name = "textBox1_Result";
+            this.textBox1_Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1_Result.Size = new System.Drawing.Size(1166, 263);
+            this.textBox1_Result.TabIndex = 5;
+            // 
+            // button_check_updates
+            // 
+            this.button_check_updates.Location = new System.Drawing.Point(277, 29);
+            this.button_check_updates.Name = "button_check_updates";
+            this.button_check_updates.Size = new System.Drawing.Size(106, 23);
+            this.button_check_updates.TabIndex = 7;
+            this.button_check_updates.Text = "Check Updates";
+            this.button_check_updates.UseVisualStyleBackColor = true;
+            this.button_check_updates.Click += new System.EventHandler(this.button2_Check_Updates);
+            // 
+            // label_HMAC_SHA256_Patch_Pkg_URL_Key
+            // 
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.AutoSize = true;
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.Location = new System.Drawing.Point(12, 9);
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.Name = "label_HMAC_SHA256_Patch_Pkg_URL_Key";
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.Size = new System.Drawing.Size(183, 13);
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.TabIndex = 9;
+            this.label_HMAC_SHA256_Patch_Pkg_URL_Key.Text = "HMAC-SHA256 Patch Pkg URL Key:";
+            // 
+            // textBox_HMAC_SHA256_Patch_Pkg_URL_Key
+            // 
+            this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key.Location = new System.Drawing.Point(201, 6);
+            this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key.Name = "textBox_HMAC_SHA256_Patch_Pkg_URL_Key";
+            this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key.Size = new System.Drawing.Size(424, 20);
+            this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key.TabIndex = 8;
+            // 
+            // linkLabel_github
+            // 
+            this.linkLabel_github.AutoSize = true;
+            this.linkLabel_github.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.linkLabel_github.Location = new System.Drawing.Point(1087, 9);
+            this.linkLabel_github.Name = "linkLabel_github";
+            this.linkLabel_github.Size = new System.Drawing.Size(89, 13);
+            this.linkLabel_github.TabIndex = 61;
+            this.linkLabel_github.TabStop = true;
+            this.linkLabel_github.Text = "Access on github";
+            this.linkLabel_github.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_github_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1113, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Version: 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 332);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel_github);
+            this.Controls.Add(this.label_HMAC_SHA256_Patch_Pkg_URL_Key);
+            this.Controls.Add(this.textBox_HMAC_SHA256_Patch_Pkg_URL_Key);
+            this.Controls.Add(this.button_check_updates);
+            this.Controls.Add(this.textBox1_Result);
+            this.Controls.Add(this.label_TitleID);
+            this.Controls.Add(this.textBox1_TitleId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download PS4 Game Update";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_TitleId;
+        private System.Windows.Forms.Label label_TitleID;
+        private System.Windows.Forms.TextBox textBox1_Result;
+        private System.Windows.Forms.Button button_check_updates;
+        private System.Windows.Forms.Label label_HMAC_SHA256_Patch_Pkg_URL_Key;
+        private System.Windows.Forms.TextBox textBox_HMAC_SHA256_Patch_Pkg_URL_Key;
+        private System.Windows.Forms.LinkLabel linkLabel_github;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ download ps4 game update/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace download_ps4_game_update
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Check_Updates(object sender, EventArgs e)
+        {
+            if (textBox_HMAC_SHA256_Patch_Pkg_URL_Key.Text.Length != 64)
+            {
+                MessageBox.Show($"HMAC_SHA256_Patch_Pkg_URL_Key invalid\nMust contain 64 characters!");
+                return;
+            }
+
+            string TitleId = textBox1_TitleId.Text;
+            if (TitleId.Length != 9)
+            {
+                MessageBox.Show($"Title ID: {TitleId} invalid\nMust contain 9 characters!\nCorrect example: CUSA00001");
+                return;
+            }
+
+            string xmlUrl = $"https://gs-sec.ww.np.dl.playstation.net/plo/np/{TitleId}/{GetHash(TitleId)}/{TitleId}-ver.xml";
+
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlUrl);
+
+                XmlNode titleNode = xmlDoc.SelectSingleNode("/titlepatch/tag/package[@version]");
+                string title = titleNode.SelectSingleNode("paramsfo/title").InnerText;
+                string version = titleNode.Attributes["version"].Value;
+
+                double sizeBytes = Convert.ToDouble(titleNode.Attributes["size"].Value);
+                double sizeMB = sizeBytes / (1024 * 1024);
+
+                XmlNode system_ver = xmlDoc.SelectSingleNode("/titlepatch/tag/package/@system_ver");
+                string hexadecimal = int.Parse(system_ver.Value).ToString("X8");
+                string system_ver_formatted = $"{hexadecimal.Substring(0, 2)}.{hexadecimal.Substring(2, 2)}";
+
+                textBox1_Result.Text = "";
+                textBox1_Result.Text += $"Title: {title}\r\n";
+                textBox1_Result.Text += $"Version: {version}\r\n";
+                textBox1_Result.Text += $"Required Firmware: {system_ver_formatted}\r\n";
+
+                Action<double> displaySize = size =>
+                    textBox1_Result.Text += $"PKG Size: {(size > 1024 ? $"{size / 1024:0.##} GB" : $"{size:0.##} MB")}\r\n\r\n";
+                displaySize(sizeMB);
+
+                XmlNode manifestNode = xmlDoc.SelectSingleNode("/titlepatch/tag/package/@manifest_url");
+                if (manifestNode == null)
+                {
+                    throw new Exception("Manifest URL not found in XML.");
+                }
+
+                using (var client = new WebClient())
+                {
+                    string json = client.DownloadString(manifestNode.Value);
+
+                    int index = json.IndexOf("\"pieces\":");
+                    if (index < 0)
+                    {
+                        throw new Exception("Expected JSON format not found.");
+                    }
+                    json = json.Substring(index + "\"pieces\":".Length);
+
+                    int urlStartIndex = json.IndexOf("\"url\":\"");
+                    while (urlStartIndex >= 0)
+                    {
+                        urlStartIndex += "\"url\":\"".Length;
+                        int urlEndIndex = json.IndexOf("\"", urlStartIndex);
+                        string packageUrl = json.Substring(urlStartIndex, urlEndIndex - urlStartIndex);
+
+                        textBox1_Result.Text += $"{packageUrl}\r\n";
+
+                        urlStartIndex = json.IndexOf("\"url\":\"", urlEndIndex);
+                    }
+                }
+                //textBox1_Result.Text += $"\r\nxmlUrl: {xmlUrl}\r\n";
+                //textBox1_Result.Text += $"manifestUrl: {manifestUrl}\r\n";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading and parsing XML: {ex.Message}");
+            }
+            finally
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = null;
+            }
+        }
+
+        private string GetHash(string gameID)
+        {
+            byte[] byteKey = StringToByteArray(textBox_HMAC_SHA256_Patch_Pkg_URL_Key.Text);
+            byte[] dataBytes = Encoding.UTF8.GetBytes($"np_{gameID}");
+
+            using (var hmacsha256 = new HMACSHA256(byteKey))
+            {
+                byte[] hashBytes = hmacsha256.ComputeHash(dataBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            int numberChars = hex.Length;
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+        private void linkLabel_github_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/DanielSvoboda/download_ps4_game_update");
+        }
+    }
+}
